c51dcc789d8ec5cd5691d2ee07e1d5f79d091229
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-11-19 18:36:54 +0800

    refactor: replace UserNav with ThemeToggle component and update button styles

--- Frontend/components/layout/ThemeToggle/theme-toggle.tsx
+++ Frontend/components/layout/ThemeToggle/theme-toggle.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
@@ -12,10 +13,11 @@ import {
 type CompProps = {};
 export default function ThemeToggle({}: CompProps) {
   const { setTheme } = useTheme();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="rounded-full">
+        <Button variant="outline" size="icon" className="rounded-md">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
--- Frontend/components/layout/header.tsx
+++ Frontend/components/layout/header.tsx
@@ -1,15 +1,15 @@
-import React from "react";
 import { SidebarTrigger } from "../ui/sidebar";
 import { Separator } from "../ui/separator";
 import { Breadcrumbs } from "../breadcrumbs";
 import SearchInput from "../search-input";
-import { UserNav } from "./user-nav";
+
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger
 } from "../ui/tooltip";
+import ThemeToggle from "./ThemeToggle/theme-toggle";
 
 export default function Header() {
   return (
@@ -33,7 +33,7 @@ export default function Header() {
         <div className="hidden md:flex">
           <SearchInput />
         </div>
-        <UserNav />
+        <ThemeToggle />
       </div>
     </header>
   );
