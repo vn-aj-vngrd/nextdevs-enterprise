6598b0a55914cdc3cf52c7da8432f87f8c8caf49
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-11-21 09:39:56 +0800

    chore: comment out NextTopLoader component in RootLayout

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -34,7 +34,7 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} overflow-hidden antialiased`}
       >
-        <NextTopLoader showSpinner={false} />
+        {/* <NextTopLoader showSpinner={false} /> */}
 
         <Providers session={session}>
           <Toaster />
