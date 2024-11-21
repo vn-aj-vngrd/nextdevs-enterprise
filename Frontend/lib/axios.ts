9ec011c1184c3664e943b9711c67fe7fda55f7b6
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-11-21 21:15:20 +0800

    feat: enhance axios response interceptor to clear cookies on 401 error and redirect to sign-in

--- Frontend/lib/axios.ts
+++ Frontend/lib/axios.ts
@@ -26,15 +26,33 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response && error.response.status === 401) {
-//       localStorage.clear();
-//       window.location.href = "/";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      if (typeof window !== "undefined") {
+        localStorage.clear();
+
+        // Delete all cookies
+        deleteAllCookies();
+      }
+      window.location.href = "/auth/sign-in";
+    }
+    return Promise.reject(error);
+  }
+);
+
+function deleteAllCookies(): void {
+  // Get all cookies as a string
+  const cookies = document.cookie.split(";");
+
+  // Iterate through each cookie
+  for (const cookie of cookies) {
+    const cookieName = cookie.split("=")[0].trim();
+
+    // Set the cookie with an expired date
+    document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/`;
+  }
+}
 
 export default axiosInstance;
