67c4da89fb10d7e1e4f54ad650a97817c70e4574
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2024-11-22 18:46:09 +0800

    fix: enhance SetAccessToken component to sign out unauthenticated users

--- Frontend/components/set-access-token.tsx
+++ Frontend/components/set-access-token.tsx
@@ -2,16 +2,21 @@
 
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
+import { signOut } from "next-auth/react";
 
 export default function SetAccessToken() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   useEffect(() => {
     const accessToken = session?.user.accessToken;
     if (accessToken) {
       localStorage.setItem("accessToken", accessToken);
     }
-  }, [session]);
+
+    if (status === "unauthenticated") {
+      signOut();
+    }
+  }, [session, status]);
 
   return null;
 }
